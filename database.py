22e755b19f9a01bda146a7ca7a203b944a883f3e
IssacPrada <90575389+IssacPrada@users.noreply.github.com>
2026-06-30 02:20:19 -0500

    Update database.py

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ class Database:
         self.pool = None
 
     async def init(self):
-        self.pool = await asyncpg.create_pool(DATABASE_URL, ssl="require")
+        pool = await asyncpg.create_pool(DATABASE_URL, ssl="require", statement_cache_size=0)
         async with self.pool.acquire() as conn:
             await conn.execute("""
                 CREATE TABLE IF NOT EXISTS strikes (
